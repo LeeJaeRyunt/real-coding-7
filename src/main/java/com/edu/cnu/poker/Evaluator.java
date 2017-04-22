af2e6e7c0364ec0ddc27c9f2f519091825bbd020
jaeryung <ii9793@daum.net>
2017-04-22 21:28:41 +0900

    onepair

--- src/main/java/com/edu/cnu/poker/Evaluator.java
+++ src/main/java/com/edu/cnu/poker/Evaluator.java
@@ -12,46 +12,23 @@ public class Evaluator {
     public String evaluate(List<Card> cardList) {
         Map<Suit, Integer> tempMap = new HashMap<Suit, Integer>();
 
-        for(Card card : cardList) {
+        for (Card card : cardList) {
             if (tempMap.containsKey(card.getSuit())) {
                 Integer count = tempMap.get(card.getSuit());
                 count = new Integer(count.intValue() + 1);
-                tempMap.put(card.getSuit(), count);
+                tempMap.put(card.getSuit(), count);g
             } else {
                 tempMap.put(card.getSuit(), new Integer(1));
             }
         }
 
-        for ( Suit key : tempMap.keySet()) {
+        for (Suit key : tempMap.keySet()) {
             if (tempMap.get(key) == 5) {
                 return "FLUSH";
             }
-         }
 
-
-
-
-    Map<Integer, Integer> tempMap1 = new HashMap<Integer, Integer>();
-    for(Card card : cardList) {
-        if (tempMap1.containsKey(card.getRank())) {
-            Integer count = tempMap1.get(card.getRank());
-            count = new Integer(count.intValue() + 1);
-            tempMap1.put(card.getRank(), count);
-        } else {
-            tempMap1.put(card.getRank(), new Integer(1));
         }
-        }
-        for ( Integer key : tempMap1.keySet())
-
-    {
-        if (tempMap1.get(key) == 1) {
-            return "ONE PAIR";
-        }
-    }
-    
-    return "NOTHING";
-
-
+        return "NOTHING";
     }
 }
 
